c75a603a89edb022ff9d9fc122963c5b3b634f61
alishajain <jain.alisha14@gmail.com>
2022-07-02 18:21:26 +0530

    Adding Auth Component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,57 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { useContext } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-import AllNotes from './pages/AllNotes';
-import NoteDetail from './pages/NoteDetails';
-import NewNote from './pages/NewNote';
-import NotFound from './pages/NotFound';
-import Layout from './components/layout/Layout';
-import ArchieveNote from './pages/ArchieveNote';
-import AllTrashNotes from './pages/AllTrashNotes';
+import AllNotes from "./pages/AllNotes";
+import NoteDetail from "./pages/NoteDetails";
+import NewNote from "./pages/NewNote";
+import Layout from "./components/layout/Layout";
+import ArchieveNote from "./pages/ArchieveNote";
+import AllTrashNotes from "./pages/AllTrashNotes";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
-          <Redirect to='/notes' />
+        <Route path="/" exact>
+          <HomePage />
         </Route>
-        <Route path='/notes' exact>
-          <AllNotes />
+        {!authCtx.isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        <Route path="/profile">
+          {authCtx.isLoggedIn && <UserProfile />}
+          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
         </Route>
-        <Route path='/notes/:noteId'>
-          <NoteDetail />
+        <Route path="/notes" exact>
+          {authCtx.isLoggedIn && <AllNotes />}
+          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
         </Route>
-        <Route path='/new-note'>
-          <NewNote />
+        <Route path="/notes/:noteId">
+          {authCtx.isLoggedIn && <NoteDetail />}
+          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
         </Route>
-        <Route path='/archieve'>
-          <ArchieveNote />
+        <Route path="/new-note">
+          {authCtx.isLoggedIn && <NewNote />}
+          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
         </Route>
-        <Route path='/trash'>
-          <AllTrashNotes />
+        <Route path="/archieve">
+          {authCtx.isLoggedIn && <ArchieveNote />}
+          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
         </Route>
-        <Route path='*'>
-          <NotFound />
+        <Route path="/trash">
+          {authCtx.isLoggedIn && <AllTrashNotes />}
+          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -1,32 +1,77 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+
 import "./MainNav.scss";
+import AuthContext from "../../store/auth-context";
 
 const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    console.log("alisha");
+    authCtx.logout();
+  };
+
   return (
     <header className="header">
       <div className="logo">Daily Notes</div>
+
       <nav className="nav">
         <ul>
-          <li>
-            <NavLink to="/notes" activeClassName="active">
-              All Notes
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/new-note" activeClassName="active">
-              Add Note
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/archieve" activeClassName="active">
-              Archieve
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/trash" activeClassName="active">
-              Trash
-            </NavLink>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <NavLink to="/auth" activeClassName="active">
+                Login
+              </NavLink>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <NavLink to="/profile" activeClassName="active">
+                Profile
+              </NavLink>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <NavLink to="/notes" activeClassName="active">
+                All Notes
+              </NavLink>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <NavLink to="/new-note" activeClassName="active">
+                Add Note
+              </NavLink>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <NavLink to="/archieve" activeClassName="active">
+                Archieve
+              </NavLink>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <NavLink to="/trash" activeClassName="active">
+                Trash
+              </NavLink>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
